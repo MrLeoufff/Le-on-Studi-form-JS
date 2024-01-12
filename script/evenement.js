601576cea7bd503b8180e47b1445b27177097f35
MrLeoufff <mrleoufff@gmail.com>
2024-01-12 12:57:27 +0100

    final

--- script/evenement.js
+++ script/evenement.js
@@ -1,3 +1,35 @@
-export default class Formulaire {
+import Formulaire  from "./formulaire.js";
 
-}
+// Créer le formulaire
+
+const formulaire = new Formulaire("formulaire");
+
+formulaire.maskChamp("societe");
+
+formulaire.maskChamp("email");
+
+// addeventlistener pour changer le comportement en fonction du radio coché
+
+formulaire.getElement("particulier").addEventListener("change", () => {
+    formulaire.hideChamp("societe")
+});
+
+formulaire.getElement("professionnel").addEventListener("change", () => {
+    formulaire.showChamp("societe")
+});
+
+// addevenlistener pour changer le comportement en fonction de objet
+
+formulaire.getElement("objet").addEventListener("change", () => {
+    formulaire.isSelected("objet", "demande_de_contact", () => {formulaire.showChamp("email")}, () => formulaire.hideChamp ())
+});
+
+// addeventlistener pour récupérer les données du formulaire
+
+formulaire.formulaireHtml.addEventListener("submit",
+    (event) => {
+        event.preventDefault();
+        formulaire.affAnswers();
+    }
+)
+
